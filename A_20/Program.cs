259174bd160c624b748a178c041f6c095b22397c
Piotrek <@>
2017-10-26 15:53:44 +0200

    przesłanie plików

---
+++ A_20/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę");
+            int a = Convert.ToInt32(Console.ReadLine());
+            for (int i = a; i > 0; i--)
+            {
+                for (int j = 0; j < i - 1; j++)
+                {
+                    Console.Write(a);
+                }
+                Console.WriteLine(a);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+Napisz program wczytujący z klawiatury liczbę dodatnią. Następnie na konsoli wyświetl trójkąt prostokątny o długości boku równej tej liczbie.
+
+Przykładowe wejście: 
+
+5
+
+Przykładowe wyjście:
+
+55555
+
+5555
+
+555
+
+55
+
+5
